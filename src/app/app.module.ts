3efcd3ba7f97d586bdd178acc059ce2ad089ba54
justinthadathil <justinthadathil93@gmail.com>
2023-06-14 14:02:34 +0100

    refactor code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,7 +7,6 @@ import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //components
-import { AppWelcomeComponent } from './components/app-welcome/app-welcome.component';
 import { AppListingComponent } from './components/app-listing/app-listing.component';
 import { AppCreateTaskComponent } from './components/app-create-task/app-create-task.component';
 import { AppFilterComponent } from './components/app-filter/app-filter.component';
@@ -16,14 +14,12 @@ import { AppFilterComponent } from './components/app-filter/app-filter.component
 @NgModule({
   declarations: [
     AppComponent,
-    AppWelcomeComponent,
     AppListingComponent,
     AppCreateTaskComponent,
     AppFilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgbModule,
     ReactiveFormsModule,
     FormsModule,
